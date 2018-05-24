f7039d9f43f2a9d520170f39410a47795db0ff23
martynas19911029 <martynas19911029@gmail.com>
2018-05-24 10:59:35 +0300

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">  
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sodyba Prie Alaušo</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="small-business.css" rel="stylesheet">
+      
+  </head>
+
+  <body>
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">Pradžia</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="imagesgal.php">Galerija
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Apie Mus</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Nuoma</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Kontaktai</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
